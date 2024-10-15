a60011d75180e96430b5dc6ba512bf1fe9ee02ce
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-10-15 17:08:59 +0600

    Increment js

--- 01_React_Fundamental/JS/increment.js
+++ 01_React_Fundamental/JS/increment.js
@@ -1,5 +1,16 @@
 const domContainer = document.querySelector("#root");
 
+const Increment = () => {
+  return (
+    <div>
+      <h1 id="display">0</h1>
+        <div>
+          <button id="button">Increment +</button>
+        </div>    
+  </div>
+  )
+}
+
 const myElement = (
   <div>
     <h1 id="display">0</h1>
