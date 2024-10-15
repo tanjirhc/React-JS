c68d10656b89265ed970e9e19e51922a5d21fd4a
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-10-15 16:50:00 +0600

    normal dom

--- 01_React_Fundamental/JS/increment.js
+++ 01_React_Fundamental/JS/increment.js
@@ -1,7 +1,11 @@
 const domContainer = document.querySelector("#root");
 
-const myElement = React.createElement("div", null, [React.createElement("p", null, "Hello World"),
-  React.createElement("p", null, "Hello World")
-]);
+// const myElement = React.createElement("div", null, [React.createElement("p", null, "Hello World"),
+//   React.createElement("p", null, "Hello World")
+// ]);
 
-ReactDOM.render(myElement, domContainer);
+// ReactDOM.render(myElement, domContainer);
+
+let p = document.createElement("p");
+p.innerHTML = "Hello World";
+domContainer.appendchild(p);
