ffdbe941525e0f863b063de3dcbddb59030b033d
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-10-15 17:22:16 +0600

    onClick

--- 01_React_Fundamental/JS/increment.js
+++ 01_React_Fundamental/JS/increment.js
@@ -1,12 +1,13 @@
 const domContainer = document.querySelector("#root");
 
 const Increment = () => {
+  const [counter, setCounter] = React.useState(0);
   
   return (
     <div>
-      <h1 id="display">0</h1>
+      <h1 id="display">{ counter }</h1>
         <div>
-          <button id="button">Increment +</button>
+          <button id="button" onClick={ () => setCounter(counter + 1)}>Increment +</button>
         </div>    
     </div>
   );
@@ -16,12 +17,4 @@ const Increment = () => {
  ReactDOM.render(<Increment />, domContainer);
 
 
- let number = 0;
 
-const button = document.querySelector("#button");
-const display = document.querySelector("#display");
-
-button.addEventListener("click", () => {
-  number++;
-  display.textContent = number;
-});
