5be4dfc14d4295fdb07a2b114bee49fb47da8391
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-10-15 17:14:10 +0600

    ascending descending from script.js

--- 01_React_Fundamental/JS/increment.js
+++ 01_React_Fundamental/JS/increment.js
@@ -13,5 +13,15 @@ const Increment = () => {
 };
 
 
-
  ReactDOM.render(<Increment />, domContainer);
+
+
+ let number = 0;
+
+const button = document.querySelector("#button");
+const display = document.querySelector("#display");
+
+button.addEventListener("click", () => {
+  number++;
+  display.textContent = number;
+});
