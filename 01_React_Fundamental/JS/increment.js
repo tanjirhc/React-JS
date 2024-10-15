fd7dae246f21a2bc1f7f3849e8f10838ba163770
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-10-15 17:11:06 +0600

    Increment() function calling

--- 01_React_Fundamental/JS/increment.js
+++ 01_React_Fundamental/JS/increment.js
@@ -1,23 +1,17 @@
 const domContainer = document.querySelector("#root");
 
 const Increment = () => {
+  
   return (
     <div>
       <h1 id="display">0</h1>
         <div>
           <button id="button">Increment +</button>
         </div>    
-  </div>
-  )
-}
+    </div>
+  );
+};
 
-const myElement = (
-  <div>
-    <h1 id="display">0</h1>
-      <div>
-        <button id="button">Increment +</button>
-      </div>    
-  </div>
-);
 
- ReactDOM.render(myElement, domContainer);
+
+ ReactDOM.render(Increment(), domContainer);
